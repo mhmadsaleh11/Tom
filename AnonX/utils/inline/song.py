2c03c573fbcb1dcbb998badc9c562caeb7da714e
mhmadsaleh11 <167534312+mhmadsaleh11@users.noreply.github.com>
2024-04-21 03:44:23 +0300

    song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝", url=f"https://t.me/pp_g3",
+                text="كبتاغون😂Cptagon", url=f"https://t.me/Cptagon",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
